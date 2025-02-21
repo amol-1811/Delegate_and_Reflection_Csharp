0037191cfd685b7214b7fec772493c6702164f40
Amol Gosavi <gosaviamol1811@gmail.com>
2025-02-21 13:10:15 +0530

    Delegate programs Added

---
+++ ConsoleMenuSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Delegate_and_Reflection
+{
+    public delegate void MenuAction();
+    internal class ConsoleMenuSystem
+    {
+        public static void ShowDate()
+        {
+            Console.WriteLine("Current Date: "+ DateTime.Now.ToShortDateString());
+        }
+
+        public static void ShowTime()
+        {
+            Console.WriteLine("Current Time: " + DateTime.Now.ToShortTimeString());
+        }
+
+        public static void Exit()
+        {
+            Console.WriteLine("Exiting the application...");
+        }
+
+        public static void Show()
+        {
+            Dictionary<string, MenuAction> menu = new Dictionary<string, MenuAction>
+            {
+                {"1", ShowDate},
+                {"2", ShowTime},
+                {"3", Exit}
+            };
+
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Show Date");
+                Console.WriteLine("2. Show Time");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                if (menu.ContainsKey(choice))
+                {
+                    menu[choice]();
+                    if (choice == "3") break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Delegate_and_Reflection;
+
+public delegate void PrintMessage(string message);
+public class Program
+{
+    public static void PrintToConsole(string message)
+    {
+        Console.WriteLine(message);
+    }
+    public static void PrintToFile(string message)
+    {
+        Console.WriteLine("File: "+message);
+    }
+    public static void Main(string[] args)
+    {
+        //PrintMessage printMessage = new PrintMessage(PrintToConsole);
+        //printMessage("Hello, World!");
+        //PrintMessage printMessageDel = PrintToConsole;
+        //printMessageDel += PrintToFile;
+
+        //printMessageDel("Hello, Multicast!");
+        ConsoleMenuSystem.Show();
+    }
+}
